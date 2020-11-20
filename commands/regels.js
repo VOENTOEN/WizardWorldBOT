a5d647399ea3e5a942153dd6232ce952a3fd119b
VOENTOEN <74774800+VOENTOEN@users.noreply.github.com>
2020-11-20 19:29:19 +0100

    Update regels.js

--- commands/regels.js
+++ commands/regels.js
@@ -3,9 +3,9 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) =>{
 
     var embed = new discord.MessageEmbed()
-        .addfield(name="1.", value="Je bent verantwoordelijk voor je eigen account. (Dit wilt zeggen dat excuses met het was mijn broer, zusje, kat, hond, geest geen geldig excuus zijn.)", inline=False)
-        .addfield(name="2.", value="Je account is op een legale wijze aangemaakt. (Geen cracked, gestolen account)", inline=True)
-        .addfield(name="3.", value="Een ban omzeilen met een ander account is niet toegestaan, vraag netjes een unban aan bij  een admin. ", inline=True)
+        .addfield(name="1.", value="Je bent verantwoordelijk voor je eigen account. (Dit wilt zeggen dat excuses met het was mijn broer, zusje, kat, hond, geest geen geldig excuus zijn.)")
+        .addfield(name="2.", value="Je account is op een legale wijze aangemaakt. (Geen cracked, gestolen account)")
+        .addfield(name="3.", value="Een ban omzeilen met een ander account is niet toegestaan, vraag netjes een unban aan bij  een admin. ")
 
     }
 
