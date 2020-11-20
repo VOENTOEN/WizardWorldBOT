568b360639befc9888beb37f40f108ada0ebd7b1
VOENTOEN <74774800+VOENTOEN@users.noreply.github.com>
2020-11-20 19:36:40 +0100

    Update regels.js

--- commands/regels.js
+++ commands/regels.js
@@ -3,8 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) =>{
 
     var embed = new discord.MessageEmbed()
-    .addField("Je bent verantwoordelijk voor je eigen account. (Dit wilt zeggen dat excuses met het was mijn broer, zusje, kat, hond, geest geen geldig excuus zijn.)");
-    }
+    .setTitle("Jouw account")
+    .setColor("#ff0000")
+    .addField("1. Je bent verantwoordelijk voor je eigen account. (Dit wilt zeggen dat excuses met het was mijn broer, zusje, kat, hond, geest geen geldig excuus zijn.)");
+    
+    channel.send(embed);
+}
 
 module.exports.help = {
     name: "regels"
