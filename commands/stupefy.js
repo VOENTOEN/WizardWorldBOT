66861bc9d1647a71068415e90dffc058800f8be3
VOENTOEN <74774800+VOENTOEN@users.noreply.github.com>
2020-11-20 16:25:10 +0100

    Create stupefy.js

---
+++ commands/stupefy.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) =>{
+
+    client.message.send("Stupefy! https://gfycat.com/remorsefulhorriblecod")
+
+    }
+
+module.exports.help = {
+    name: "stupefy"
+}
