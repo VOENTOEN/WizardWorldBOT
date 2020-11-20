085b346da32ba3b6429a5d16ce9c0014b72171bb
VOENTOEN <74774800+VOENTOEN@users.noreply.github.com>
2020-11-20 16:26:43 +0100

    Update stupefy.js

--- commands/stupefy.js
+++ commands/stupefy.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) =>{
 
-    client.message.send("Stupefy! https://gfycat.com/remorsefulhorriblecod")
+    client.message.send('Stupefy! https://gfycat.com/remorsefulhorriblecod')
 
     }
 
