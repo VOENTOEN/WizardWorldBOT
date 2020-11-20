9e120b7ae186febe26155fcf5bbc3d8412b244f2
VOENTOEN <74774800+VOENTOEN@users.noreply.github.com>
2020-11-20 16:33:29 +0100

    Create expelliarmus.js

---
+++ commands/expelliarmus.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) =>{
+
+    message.channel.send("Expelliarmus! https://gfycat.com/ethicalcomplexjunebug");
+
+    }
+
+module.exports.help = {
+    name: "expelliarmus"
+}
